e14b24a3552698f561b900be3269b2b09d08f33d
udbhavghimire <ghimireudbhav@gmail.com>
2022-02-08 21:09:03 +0545

    connection

--- admin/loginsystem/includes/dbh.inc.php
+++ admin/loginsystem/includes/dbh.inc.php
@@ -1,16 +1,16 @@
 <?php 
 
-	$dbservername = "localhost";
-	$dbusername = "root";
-	$dbpassword = "";
-	$dbname = "trianglesolution";
-	$conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
-	
 	// $dbservername = "localhost";
-	// $dbusername = "u295830655_tri";
-	// $dbpassword = "@#Tri123@#";
-	// $dbname = "u295830655_triangle";
-	// $conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname); 
+	// $dbusername = "root";
+	// $dbpassword = "";
+	// $dbname = "trianglesolution";
+	// $conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
+	
+	$dbservername = "localhost";
+	$dbusername = "u295830655_tri";
+	$dbpassword = "@#Tri123@#";
+	$dbname = "u295830655_triangle";
+	$conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname); 
 
  ?>
 
